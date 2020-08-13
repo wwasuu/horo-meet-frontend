a10e08a418065aae339795c1ae16e63d1c236074
wasu winitmontri <wasu.win@ascendcorp.com>
2020-08-13 13:14:57 +0700

    update

--- src/pages/prediction.js
+++ src/pages/prediction.js
@@ -1,14 +1,46 @@
-import React from "react"
-import Particles from "react-particles-js";
-import Layout from "../component/Layout"
+import React from "react";
+import { useSelector } from "react-redux";
+import Layout from "../component/Layout";
 import "../styles/app.scss";
 
 const Prediction = () => {
+  const user = useSelector((state) => state.user);
   return (
     <Layout>
       <div className="layout">
-        <div className="content__container">
+        <div className="content__container content__container--prediction">
           <h1 className="title">ผลทำนาย</h1>
+          <div className="subtitle subtitle--center">ธาตุไม้</div>
+          <div className="">
+            <div className="meta">
+              <i className="fal fa-briefcase" /> อาชีพ
+            </div>
+            <div>{user.occupation}</div>
+          </div>
+          <div>
+            <div className="meta">
+              <i className="fal fa-bed" /> หันหัวนอน
+            </div>
+            <div>{user.sleep_bed}</div>
+          </div>
+          <div>
+            <div className="meta">
+              <i className="fal fa-desktop" /> โต๊ะทำงาน
+            </div>
+            <div>{user.work_desk}</div>
+          </div>
+          <div >
+            <div className="meta">
+              <i className="fal fa-palette" /> สีนำโชค
+            </div>
+            <div>{user.color}</div>
+          </div>
+          <div className="">
+            <div className="meta">
+              <i className="fal fa-hiking" /> กิจกรรมเสริมดวง
+            </div>
+            <div>{user.activity}</div>
+          </div>
         </div>
       </div>
     </Layout>
