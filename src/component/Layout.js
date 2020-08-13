7fdf453ed74ceea4ec13b2dd99608813a2865797
wasu winitmontri <wasu.win@ascendcorp.com>
2020-08-13 12:32:51 +0700

    update

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -1,31 +1,59 @@
-import React from "react"
+import React from "react";
 import cn from "classnames";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory, Link, useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
   const history = useHistory();
+  const location = useLocation();
 
   function signout() {
     history.push("/");
   }
 
+  console.log(location);
+
   return (
     <div>
       <div className="navbar">
-        <div className="navbar__item">
+        <div
+          className={cn("navbar__item", {
+            "navbar__item--active": location.pathname === "/prediction",
+          })}
+        >
           <Link to="/prediction">
-            <i className="fas fa-star fa-lg" />
-          </Link>
+            <i
+              className={cn("fal fa-star fa-lg", {
+                fas: location.pathname === "/prediction",
+              })}
+            />
+          </Link>          
         </div>
-        <div className="navbar__item">
+        <div
+          className={cn("navbar__item", {
+            "navbar__item--active": location.pathname === "/calendar",
+          })}
+        >
           <Link to="/calendar">
-            <i className="fal fa-calendar-star fa-lg" />
+            <i
+              className={cn("fal fa-calendar-star fa-lg", {
+                fas: location.pathname === "/calendar",
+              })}
+            />
           </Link>
         </div>
-        <div className="navbar__item">
+        <div
+          className={cn("navbar__item", {
+            "navbar__item--active": location.pathname === "/partner",
+          })}
+        >
           <Link to="/partner">
-            <i className="fal fa-handshake fa-lg" />
+            <i
+              className={cn("fal fa-handshake fa-lg", {
+                fas: location.pathname === "/partner",
+              })}
+            />
           </Link>
+     
         </div>
         <div className="navbar__item" onClick={signout}>
           <i className="fal fa-sign-out fa-lg" />
